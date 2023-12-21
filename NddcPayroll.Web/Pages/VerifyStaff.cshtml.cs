1bd2c0ee0241641525f031a8b36ea5af82e230f1
Dash Dogg <advantellocloud@gmail.com>
2023-12-21 11:11:40 +0100

    update

---
+++ NddcPayroll.Web/Pages/VerifyStaff.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace NddcPayroll.Web.Pages
+{
+    public class VerifyStaffModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
--- NddcPayroll.Web/Program.cs
+++ NddcPayroll.Web/Program.cs
@@ -53,7 +53,7 @@ builder.Services.AddRazorPages(options => {
     options.Conventions.AllowAnonymousToPage("/Index");
     options.Conventions.AllowAnonymousToPage("/Reports/PayrollSummaryByDept");
     options.Conventions.AllowAnonymousToPage("/PdfPages/EmployeePayslip/");
-    options.Conventions.AllowAnonymousToPage("/Identity/VerifyStaff/");
+    options.Conventions.AllowAnonymousToPage("/VerifyStaff");
 })
 .AddMvcOptions(options => { })
 .AddMicrosoftIdentityUI();
